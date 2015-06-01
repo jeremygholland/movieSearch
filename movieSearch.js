e24f4eccbe085c9adf1556a270eace0fcfc1ca44
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-06-01 13:01:19 -0500

    added movie search functionality

--- movieSearch.js
+++ movieSearch.js
@@ -20,16 +20,25 @@ if (Meteor.isClient) {
 
   Template.body.events({
     'submit .firstOne': function () {
+      var actor1= [];
       // increment the counter when button is clicked
       var text = event.target.text.value;
       console.log(text);
      $.getJSON("http://api.themoviedb.org/3/search/person?api_key=bc8d6eb8376311f84080885ca58a2fd8&query= "+ text).then(function (json){
-      alert(json.results[0].id);
+      var id = json.results[0].id;
+      console.log(id);
+      $.getJSON("http://api.themoviedb.org/3/person/"+id+"/movie_credits?api_key=bc8d6eb8376311f84080885ca58a2fd8").then(function (data){
+       for (i = 0; i <data.cast.length; i++){
+       actor1.push(data.cast[i].id);
+
+     }
+     console.log(actor1);
      })
+  });
      event.target.text.value = '';
       return false;
-  }
-  });
+}
+})
 }
 
 Meteor.methods({
